079b223d5269bed10959a5d85b2c2598289450a5
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-22 12:48:02 -0400

    added create tables to schema.sql

--- README.md
+++ README.md
@@ -34,11 +34,7 @@ To add a screenshot, create an `assets/images` folder in your repository and upl
 
 ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
+https://www.w3schools.com/sql/sql_datatypes.asp
 
 ## License
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,21 @@
 DROP DATABASE IF EXISTS techBlog_db;
 CREATE DATABASE techBlog_db;
+
+USE techBlog_db;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE post (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    description LONGTEXT NOT NULL,
+    date_created DATE NOT NULL,
+    userId id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (userId) REFERENCES user(id)
+)
