a698f8f2df992c7ff6dcae85c278b6940e5d5926
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-24 14:55:06 -0400

    added boiler plate server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const { Sequelize } = require('sequelize');
-const hbs = exphbs.create({});
+
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
