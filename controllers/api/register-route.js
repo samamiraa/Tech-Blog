25f16d82094dd5e42517e3f143a97e71ad1592de
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-26 20:17:29 -0400

    added route to add user to db

--- controllers/api/register-route.js
+++ controllers/api/register-route.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
+const { User } = require('../../models')
 
 router.get('/', async (req, res) => {
     res.render('register.handlebars');
+});
+
+router.post('/', async (req, res) => {
+    console.log(req.body);
+
+    try {
+        const newUser = await User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            username: req.body.email,
+            password: req.body.password,
+        });
+    } catch (error) {
+        console.error('Error adding new user:', error);
+        return res
+            .status(500)
+            .json({ message: 'Internal Server Error adding new user!' });
+    };
 });
 
 module.exports = router;
--- public/js/register.js
+++ public/js/register.js
@@ -1,17 +1,27 @@
 const registerBtn = document.getElementById('register-btn');
 
-
 registerBtn.addEventListener('click', function (event) {
     event.preventDefault();
 
     const firstName = document.getElementById('first-name').value;
-    const lastname = document.getElementById('last-name').value;
+    const lastName = document.getElementById('last-name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const passwordConfirm = document.getElementById('confirmPassword').value;
 
     console.log('Register Button has clicked!');
-    console.log('name: ' + firstName + lastname);
+    console.log('name: ' + firstName + lastName);
     console.log('email: ' + email);
-    console.log('password: ' + password + passwordConfirm);
+    console.log('password: ' + password);
+
+    const user = { firstName, lastName, email, password };
+
+    const postRequest = axios.post('/api/register', user);
+
+    postRequest
+        .then(function (response) {
+            console.log('success!', response.data)
+        })
+        .catch(function (error) {
+            console.error('Error:', error.message);
+        });
 });
