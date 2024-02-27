297b706f3d8b1593e798e0072079cee48506888d
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-27 12:22:06 -0400

    started login route

---
+++ public/js/login.js
@@ -0,0 +1,8 @@
+const loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', function (event) {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    
+});
