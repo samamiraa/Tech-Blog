4942876f057f57c9b5985b705bb53e171ae4475b
Samantha Allen <samanthaamiraallen@gmail.com>
2024-02-27 20:45:51 -0400

    moved all js scripts

--- public/js/home.js
+++
@@ -1,6 +0,0 @@
-const loginButton = document.getElementById('login-btn');
-const registerButton = document.getElementById('register-btn');
-
-// loginButton.addEventListener('click', function (event) {
-//     window.location.href('/api/login');
-// });
--- public/js/login.js
+++
@@ -1,8 +0,0 @@
-const loginBtn = document.getElementById('login-btn');
-
-// loginBtn.addEventListener('click', function (event) {
-//     // const email = document.getElementById('email').value;
-//     // const password = document.getElementById('password').value;
-
-    
-// });
